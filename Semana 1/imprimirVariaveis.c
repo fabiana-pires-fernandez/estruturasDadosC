19e67cc2b877d01ca8e940e541372b7c1b2c2e4b
fabiana-pires-fernandez <fabi.nandez@hotmail.com>
2020-08-12 13:22:53 -0300

    Imprimindo variaveis

---
+++ Semana 1/imprimirVariaveis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main( ) {
+	char a = 'B';
+	int x = 20;
+	float y = x/8;
+	float z = 1.0*x/8;
+	printf("a: %c\n", a);
+	printf("x: %i\n", x);
+	printf("y: %f\n", y);
+	printf("z: %f\n", z);
+	return 0;
+}
